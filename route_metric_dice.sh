bc1d3f873b5b967adeb13daa248f59e9ed53ac45
greg baker <gregbakerstl@gmail.com>
2020-04-02 23:03:06 -0500

    sloppy avoid collisions

--- route_metric_dice.sh
+++ route_metric_dice.sh
@@ -33,18 +33,22 @@ main() {
 
 	weight='0'
 	interfaces="$(get_interfaces)"
-	last_interface='';
+	last_interface="$1";
 
 	for interface in $interfaces; do
-		change_int_metric "$interface" "$weight"
-		echo "changed metric to prefer interface $interface over $last_interface"
-		last_interface="$interface"
-		chill '5'; 
+		if [ "$interface" != "$last_interface" ]; then
+			change_int_metric "$interface" "$weight"
+			echo "changed metric to prefer interface $interface over $last_interface"
+			last_interface="$interface"
+			chill '1'; 
+		fi
 	done
 
 }
 
 
 while true; do 
-	main
+
+	main "$last_interface"
+
 done
